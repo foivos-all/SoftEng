c59288db85d6afc05c3650ac19edfe8316801777
foivos-all <74879779+foivos-all@users.noreply.github.com>
2021-08-31 19:37:30 +0300

    Delete conn.php

--- conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db_name = "healthcare";
-    $username = "root";
-    $password = "";
-    $server_name = "localhost";
-
-    $conn = mysqli_connect($server_name, $username, $password, $db_name);
-?>
