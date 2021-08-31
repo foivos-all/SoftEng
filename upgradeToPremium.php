ea8340c36e28cb72718499126789b6b7fe17fc91
foivos-all <74879779+foivos-all@users.noreply.github.com>
2021-08-31 19:42:11 +0300

    Delete upgradeToPremium.php

--- upgradeToPremium.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require "conn.php";
-
-    $user_id = $_POST['user_id'];
-    $query = "update users set property = 'P' where id = '$user_id';";
-    $result = mysqli_query($conn, $query);
-
-    echo "Success";
-?>
